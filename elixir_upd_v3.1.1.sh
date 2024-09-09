8116039114a934ae870b0d7b2202a36f35a683d9
KrakeNocto <octo2kraken@gmail.com>
2024-09-09 13:45:52 +0300

    Update elixir_upd_v3.1.1.sh

--- elixir_upd_v3.1.1.sh
+++ elixir_upd_v3.1.1.sh
@@ -3,12 +3,14 @@
 docker kill elixir
 docker rm elixir
 
+docker pull elixirprotocol/validator:v3 --platform linux/amd64
+
 docker run -d \
   --env-file /root/.elixir/validator.env \
   --name elixir \
   -p 17690:17690 \
   --restart unless-stopped \
-  elixirprotocol/validator:3.1.1
+  elixirprotocol/validator:3.4.0
 
 rm elixir_upd_v3.1.1.sh
 
