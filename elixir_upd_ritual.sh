6d2023b74d82bf7c3216ab1c49deca31a34068d4
KrakeNocto <octo2kraken@gmail.com>
2024-09-23 10:24:29 +0300

    Create elixir_upd_ritual.sh

---
+++ elixir_upd_ritual.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker kill elixir
+docker rm elixir
+
+docker pull elixirprotocol/validator:v3 --platform linux/amd64
+
+docker run -d \
+  --env-file /home/ritual/.elixir/validator.env \
+  --name elixir \
+  -p 17690:17690 \
+  --restart unless-stopped \
+  elixirprotocol/validator:3.4.6
+
+rm elixir_upd_v3.1.1.sh
+
+docker logs -f elixir
