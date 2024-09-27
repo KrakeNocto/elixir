062634152c6ed33d6b8b275bb89874a9ecc1d3c8
KrakeNocto <octo2kraken@gmail.com>
2024-09-27 15:36:01 +0300

    Update elixir_upd_v3.1.1.sh

--- elixir_upd_v3.1.1.sh
+++ elixir_upd_v3.1.1.sh
@@ -10,7 +10,7 @@ docker run -d \
   --name elixir \
   -p 17690:17690 \
   --restart unless-stopped \
-  elixirprotocol/validator:3.4.6
+  elixirprotocol/validator:3.4.7
 
 rm elixir_upd_v3.1.1.sh
 
