5f93641470e0cb0ac6c16f41a2fb938d7d4b437e
KrakeNocto <octo2kraken@gmail.com>
2024-10-04 17:04:23 +0300

    Update elixir.sh

--- elixir.sh
+++ elixir.sh
@@ -28,7 +28,7 @@ sed -i.bak -e "s|^STRATEGY_EXECUTOR_IP_ADDRESS=.*|STRATEGY_EXECUTOR_IP_ADDRESS=$
 
 
 # Download the Docker image for the Testnet v3 validator by running the following docker command:
-docker pull elixirprotocol/validator:3.4.8
+docker pull elixirprotocol/validator:3.4.9
 
 # Создаем папку .elixir и закидываем файл конфига
 mkdir -p /root/.elixir
@@ -40,7 +40,7 @@ docker run -d \
   --name elixir \
   -p 17690:17690 \
   --restart unless-stopped \
-  elixirprotocol/validator:3.4.8
+  elixirprotocol/validator:3.4.9
 
   rm elixir.sh
 
