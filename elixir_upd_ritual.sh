eb43ae8a4f4ac73c3f8c4a60883f39c2d6ef2cb4
KrakeNocto <octo2kraken@gmail.com>
2024-10-09 13:43:38 +0300

    Update elixir_upd_ritual.sh

--- elixir_upd_ritual.sh
+++ elixir_upd_ritual.sh
@@ -22,7 +22,7 @@ docker run -d \
   --name elixir \
   -p 17690:17690 \
   --restart unless-stopped \
-  elixirprotocol/validator:3.4.9
+  elixirprotocol/validator:3.5.0
 
 rm elixir_upd_ritual.sh
 
